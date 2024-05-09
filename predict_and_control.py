c0ccf1e5253e4fa5e0fdfbf886cd7a7b47e6178d
Vanex-h <vanessahirwa5@gmail.com>
2024-05-09 17:55:30 +0200

    FOrmat

--- predict_and_control.py
+++ predict_and_control.py
@@ -22,7 +22,7 @@ faceRectangleBorderColor = nametagColor
 faceRectangleBorderSize = 2
 
 # Open a connection to the Arduino
-ser = serial.Serial('COM11', 9600)  # Change port to your Arduino's port
+ser = serial.Serial("COM11", 9600)  # Change port to your Arduino's port
 time.sleep(2)  # Allow time for Arduino to initialize
 
 # Open a connection to the first webcam
@@ -39,82 +39,114 @@ while True:
     gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
 
     # Detect faces
-    faces = faceCascade.detectMultiScale(gray, scaleFactor=1.1, minNeighbors=5, minSize=(30, 30))
+    faces = faceCascade.detectMultiScale(
+        gray, scaleFactor=1.1, minNeighbors=5, minSize=(30, 30)
+    )
 
     # Control flag for LED
     is_access_granted = False
-    is_vip_granted= False
+    is_vip_granted = False
 
     # For each face found
-    for (x, y, w, h) in faces:
+    for x, y, w, h in faces:
 
         # Recognize the face
-        ID, Confidence = faceRecognizer.predict(gray[y:y + h, x:x + w])
+        ID, Confidence = faceRecognizer.predict(gray[y : y + h, x : x + w])
         # Confidence normalization to a 0-100 scale
         Confidence = 100 - Confidence
         if Confidence > 30:
             if ID == 0:
                 Person = "pseudo"
                 is_access_granted = False
-                is_vip_granted= False
+                is_vip_granted = False
 
             elif ID == 1:
                 Person = "Honore"
                 is_access_granted = True
-                is_vip_granted= True
+                is_vip_granted = True
             elif ID == 2:
                 Person = "Dalyoung"
                 is_access_granted = False
-                is_vip_granted= False
+                is_vip_granted = False
             elif ID == 3:
                 Person = "Godwill"
                 is_access_granted = False
-                is_vip_granted= False
+                is_vip_granted = False
             elif ID == 4:
                 Person = "Bright"
                 is_access_granted = False
-                is_vip_granted= False
+                is_vip_granted = False
             elif ID == 5:
-                Person = "Bena"   
-                is_access_granted = False 
-                is_vip_granted= False   
+                Person = "Bena"
+                is_access_granted = False
+                is_vip_granted = False
             elif ID == 7:
-                Person = "Sugira"   
-                is_access_granted = True  
-                is_vip_granted= False
+                Person = "Sugira"
+                is_access_granted = True
+                is_vip_granted = False
             elif ID == 6:
-                Person= "Vanessa"
-                is_access_granted=  True
-                is_vip_granted= False   
+                Person = "Vanessa"
+                is_access_granted = True
+                is_vip_granted = False
             elif ID == 8:
-                Person= "Cedrick"
-                is_access_granted=  True
-                is_vip_granted= False
-        
+                Person = "Cedrick"
+                is_access_granted = True
+                is_vip_granted = False
+
             # Create rectangle around the face
-            cv2.rectangle(frame, (x - 20, y - 20), (x + w + 20, y + h + 20), faceRectangleBorderColor, faceRectangleBorderSize)
+            cv2.rectangle(
+                frame,
+                (x - 20, y - 20),
+                (x + w + 20, y + h + 20),
+                faceRectangleBorderColor,
+                faceRectangleBorderSize,
+            )
 
             # Display name tag
-            cv2.rectangle(frame, (x - 22, y - nametagHeight), (x + w + 22, y - 22), nametagColor, -1)
-            cv2.putText(frame, str(Person) + ": " + str(round(Confidence, 2)) + "%", (x, y-fontBottomMargin), fontFace, fontScale, fontColor, fontWeight)
+            cv2.rectangle(
+                frame,
+                (x - 22, y - nametagHeight),
+                (x + w + 22, y - 22),
+                nametagColor,
+                -1,
+            )
+            cv2.putText(
+                frame,
+                str(Person) + ": " + str(round(Confidence, 2)) + "%",
+                (x, y - fontBottomMargin),
+                fontFace,
+                fontScale,
+                fontColor,
+                fontWeight,
+            )
         else:
-            cv2.rectangle(frame, (x - 20, y - 20), (x + w + 20, y + h + 20), (0, 0, 255), 2)
-            cv2.putText(frame, "Unknown", (x, y - 10), fontFace, fontScale, (0, 0, 255), fontWeight)
+            cv2.rectangle(
+                frame, (x - 20, y - 20), (x + w + 20, y + h + 20), (0, 0, 255), 2
+            )
+            cv2.putText(
+                frame,
+                "Unknown",
+                (x, y - 10),
+                fontFace,
+                fontScale,
+                (0, 0, 255),
+                fontWeight,
+            )
             print("confidence: ", Confidence)
 
     # Control
     if is_access_granted:
-        ser.write(b'1')  # Sending '1' to Arduino to turn on LED
+        ser.write(b"1")  # Sending '1' to Arduino to turn on LED
     elif is_access_granted & is_vip_granted:
-        ser.write(b'2') #Sending '2' to Arduino to turn on Blue LED
+        ser.write(b"2")  # Sending '2' to Arduino to turn on Blue LED
     else:
-        ser.write(b'0')  # Sending '0' to Arduino to turn off LED
+        ser.write(b"0")  # Sending '0' to Arduino to turn off LED
 
     # Display the resulting frame
-    cv2.imshow('Detecting Faces...', frame)
+    cv2.imshow("Detecting Faces...", frame)
 
     # Exit loop when 'q' is pressed
-    if cv2.waitKey(1) & 0xFF == ord('q'):
+    if cv2.waitKey(1) & 0xFF == ord("q"):
         break
 
 # Release the camera
@@ -122,7 +154,7 @@ camera.release()
 
 # Close the serial connection to the Arduino
 
-ser.write(b'0')  # Sending '0' to Arduino to turn off LED
+ser.write(b"0")  # Sending '0' to Arduino to turn off LED
 ser.close()
 
 # Close all OpenCV windows
