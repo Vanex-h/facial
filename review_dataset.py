1c10a7ab8e0026a2222249bec8e87591f76c6682
Vanex-h <vanessahirwa5@gmail.com>
2024-05-09 11:08:24 +0200

    Embbeded facial recognition

--- review_dataset.py
+++ review_dataset.py
@@ -3,25 +3,30 @@ import os
 import platform
 import sys
 
+
 def display_images_in_folder(folder_path):
     # Get list of image files
-    image_files = [os.path.join(folder_path, img) for img in os.listdir(folder_path) if img.endswith('.jpg')]
-    
+    image_files = [
+        os.path.join(folder_path, img)
+        for img in os.listdir(folder_path)
+        if img.endswith(".jpg")
+    ]
+
     # Sort the files for consistency
     image_files.sort()
-    
+
     for img_file in image_files:
         img = cv2.imread(img_file)
         if img is None:
             print(f"Error loading image: {img_file}")
             continue
-        
+
         # Resize the image to double resolution
         img = cv2.resize(img, None, fx=2, fy=2, interpolation=cv2.INTER_LINEAR)
-        
+
         # Get screen resolution
         screen_width, screen_height = get_screen_resolution()
-        
+
         # Calculate window position
         window_width = img.shape[1]
         window_height = img.shape[0]
@@ -29,11 +34,12 @@ def display_images_in_folder(folder_path):
         y_position = (screen_height - window_height) // 2
 
         # Create a window and display the image
-        cv2.namedWindow('Cluster Images', cv2.WINDOW_NORMAL)
-        cv2.moveWindow('Cluster Images', x_position, y_position)
-        cv2.imshow('Cluster Images', img)
+        cv2.namedWindow("Cluster Images", cv2.WINDOW_NORMAL)
+        cv2.moveWindow("Cluster Images", x_position, y_position)
+        cv2.imshow("Cluster Images", img)
         cv2.waitKey(250)  # Display for 1 second
         cv2.destroyAllWindows()
+
 
 def get_screen_resolution():
     # Get screen resolution based on platform
@@ -41,6 +47,7 @@ def get_screen_resolution():
     screen_height = 0
     if platform.system() == "Windows":
         import ctypes
+
         user32 = ctypes.windll.user32
         screen_width = user32.GetSystemMetrics(0)
         screen_height = user32.GetSystemMetrics(1)
@@ -49,6 +56,7 @@ def get_screen_resolution():
         screen_height = 720  # default value if unable to get actual screen resolution
     elif platform.system() == "Linux":
         import subprocess
+
         try:
             output = subprocess.check_output(["xrandr"]).decode("utf-8")
             for line in output.splitlines():
@@ -60,8 +68,9 @@ def get_screen_resolution():
             pass
     return screen_width, screen_height
 
+
 if __name__ == "__main__":
-    # Get folder name 
+    # Get folder name
     folder_name = "dataset"
     folder_path = os.path.join(folder_name)
 
