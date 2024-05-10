0f61124612fd93e22be4ca4cf460525d42063513
Vanex-h <vanessahirwa5@gmail.com>
2024-05-10 11:08:24 +0200

    -

--- create_dataset.py
+++ create_dataset.py
@@ -2,11 +2,11 @@ import cv2
 import time
 
 # Detect object in video stream using Haarcascade Frontal Face
-face_cascade = cv2.CascadeClassifier('models/haarcascade_frontalface_default.xml')
+face_cascade = cv2.CascadeClassifier("models/haarcascade_frontalface_default.xml")
 time.sleep(1)
 
 # For each person, one face id
-ID = input('Enter your ID: ')
+ID = input("Enter your ID: ")
 
 # Wait for 2 seconds to be able to switch to the Webcam Window.
 print("Please get your face ready!")
@@ -39,14 +39,17 @@ while True:
     faces = face_cascade.detectMultiScale(gray, 1.1, 5)
 
     # Loop through faces
-    for (x, y, w, h) in faces:
+    for x, y, w, h in faces:
         # Draw rectangle around the face
         cv2.rectangle(image, (x, y), (x + w, y + h), (255, 0, 0), 2)
 
         # Check if enough time has passed to capture an image and if image count is less than 100
         if (time.time() - current_time) * 1000 >= interval and image_count < 300:
             # Save the captured image into the datasets folder
-            cv2.imwrite(f"dataset/data.{ID}.{int(time.time() * 1000)}.jpg", gray[y:y + h, x:x + w])
+            cv2.imwrite(
+                f"dataset/data.{ID}.{int(time.time() * 1000)}.jpg",
+                gray[y : y + h, x : x + w],
+            )
             current_time = time.time()
             image_count += 1
 
@@ -54,7 +57,7 @@ while True:
     cv2.imshow("Dataset Generating...", image)
 
     # To stop taking video, press 'q' key or if image count reaches 100
-    if cv2.waitKey(1) & 0xFF == ord('q') or image_count >= 300:
+    if cv2.waitKey(1) & 0xFF == ord("q") or image_count >= 300:
         break
 
 # Release the camera and close all windows
